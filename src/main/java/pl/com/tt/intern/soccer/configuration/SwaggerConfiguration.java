546cef36f530619515029e92c00386978495f0ec
wojcik.patryk <wojcik.patrykk@gmail.com>
2019-09-09 17:09:03 +0200

    PLUB19-109 Just add one static import

--- src/main/java/pl/com/tt/intern/soccer/configuration/SwaggerConfiguration.java
+++ src/main/java/pl/com/tt/intern/soccer/configuration/SwaggerConfiguration.java
@@ -2,10 +2,10 @@ package pl.com.tt.intern.soccer.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.PathSelectors.any;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
@@ -20,7 +20,7 @@ public class SwaggerConfiguration {
         return new Docket(SWAGGER_2)
                 .select()
                 .apis(basePackage(basePackage))
-                .paths(PathSelectors.any())
+                .paths(any())
                 .build();
     }
 
