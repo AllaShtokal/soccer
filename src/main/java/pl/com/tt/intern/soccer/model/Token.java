733bf18aaaca1a8c0b749f61df21a095ca382608
Kamil Wawer <wawerk@tt.com.pl>
2019-09-11 17:06:19 +0200

    PLUB19-154 Fixes after first CR

--- src/main/java/pl/com/tt/intern/soccer/model/Token.java
+++ src/main/java/pl/com/tt/intern/soccer/model/Token.java
@@ -7,8 +7,8 @@ import pl.com.tt.intern.soccer.model.enums.ProcessType;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
+import static java.util.UUID.randomUUID;
 import static javax.persistence.EnumType.STRING;
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -23,6 +23,7 @@ public class Token {
     @Column(name = "id")
     private Long id;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
@@ -32,17 +33,19 @@ public class Token {
     @Column(name = "process_type", nullable = false)
     private ProcessType processType;
 
+    @NotNull
     @Column(name = "uuid")
     private String uuid ;
 
-    @Column(name = "expired_date_time")
-    private LocalDateTime expiredDateTime;
+    @NotNull
+    @Column(name = "expiration_time")
+    private LocalDateTime expirationTime;
 
 
-    public Token(User user, @NotNull ProcessType processType) {
+    public Token(User user, ProcessType processType) {
         this.user = user;
         this.processType = processType;
-        this.uuid = UUID.randomUUID().toString();
-        this.expiredDateTime = LocalDateTime.now().plusHours(24);
+        this.uuid = randomUUID().toString();
+        this.expirationTime = LocalDateTime.now().plusHours(24);
     }
 }
