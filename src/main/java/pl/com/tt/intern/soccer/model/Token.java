c96cd79da2d1eb6045b76b579bc8c7d966f63ec5
Kamil Wawer <wawerk@tt.com.pl>
2019-09-11 17:24:41 +0200

    PLUB19-154 Remove ProcessType enum and pole from Token Model

--- src/main/java/pl/com/tt/intern/soccer/model/Token.java
+++ src/main/java/pl/com/tt/intern/soccer/model/Token.java
@@ -2,14 +2,12 @@ package pl.com.tt.intern.soccer.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import pl.com.tt.intern.soccer.model.enums.ProcessType;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 import static java.util.UUID.randomUUID;
-import static javax.persistence.EnumType.STRING;
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -29,11 +27,6 @@ public class Token {
     private User user;
 
     @NotNull
-    @Enumerated(STRING)
-    @Column(name = "process_type", nullable = false)
-    private ProcessType processType;
-
-    @NotNull
     @Column(name = "uuid")
     private String uuid ;
 
@@ -41,10 +34,8 @@ public class Token {
     @Column(name = "expiration_time")
     private LocalDateTime expirationTime;
 
-
-    public Token(User user, ProcessType processType) {
+    public Token(User user) {
         this.user = user;
-        this.processType = processType;
         this.uuid = randomUUID().toString();
         this.expirationTime = LocalDateTime.now().plusHours(24);
     }
--- src/main/java/pl/com/tt/intern/soccer/model/enums/ProcessType.java
+++
@@ -1,6 +0,0 @@
-package pl.com.tt.intern.soccer.model.enums;
-
-public enum ProcessType {
-    ACTIVATION,
-    PASSWORD_RESET
-}
