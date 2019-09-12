a7f160b3beaeb69d237351817ef692011aa6d34c
Łukasz Grymulski <l.grymulski@tt.com.pl>
2019-09-12 15:10:01 +0200

    refactor - unused import

--- src/main/java/pl/com/tt/intern/soccer/model/User.java
+++ src/main/java/pl/com/tt/intern/soccer/model/User.java
@@ -1,6 +1,5 @@
 package pl.com.tt.intern.soccer.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
