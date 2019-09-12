e807dfaeb071c6e219ac13943719f10494281549
wojcik.patryk <wojcik.patrykk@gmail.com>
2019-09-12 13:22:05 +0200

    PLUB19-158 Add role user to registered user

--- src/main/java/pl/com/tt/intern/soccer/service/impl/SignUpServiceImpl.java
+++ src/main/java/pl/com/tt/intern/soccer/service/impl/SignUpServiceImpl.java
@@ -7,14 +7,19 @@ import pl.com.tt.intern.soccer.model.User;
 import pl.com.tt.intern.soccer.model.UserInfo;
 import pl.com.tt.intern.soccer.payload.request.SignUpRequest;
 import pl.com.tt.intern.soccer.payload.response.SuccessfulSignUpResponse;
+import pl.com.tt.intern.soccer.service.RoleService;
 import pl.com.tt.intern.soccer.service.SignUpService;
 import pl.com.tt.intern.soccer.service.UserService;
+
+import static java.util.Collections.singleton;
+import static pl.com.tt.intern.soccer.model.enums.RoleType.ROLE_USER;
 
 @Service
 @RequiredArgsConstructor
 public class SignUpServiceImpl implements SignUpService {
 
     private final UserService userService;
+    private final RoleService roleService;
 
     @Override
     public SuccessfulSignUpResponse signUp(SignUpRequest request) throws Exception {
@@ -29,6 +34,7 @@ public class SignUpServiceImpl implements SignUpService {
                     .username(request.getUsername())
                     .email(request.getEmail())
                     .password(request.getPassword())
+                    .roles(singleton(roleService.findByType(ROLE_USER)))
                     .build();
 
             return new SuccessfulSignUpResponse("User registered successfully", userService.save(user));
