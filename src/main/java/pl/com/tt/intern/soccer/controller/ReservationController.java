7f47653fc00397a88cb9588ad360d26f4a60688d
wojcik.patryk <wojcik.patrykk@gmail.com>
2019-09-16 17:46:25 +0200

    PLUB19-178 Add @RequestMapping to reservation controller

--- src/main/java/pl/com/tt/intern/soccer/controller/ReservationController.java
+++ src/main/java/pl/com/tt/intern/soccer/controller/ReservationController.java
@@ -1,11 +1,13 @@
 package pl.com.tt.intern.soccer.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.com.tt.intern.soccer.service.ReservationService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private final ReservationService reservationService;
