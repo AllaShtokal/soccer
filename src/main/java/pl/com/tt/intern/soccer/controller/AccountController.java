83bd694d0208ce2d97b58d76bef80c5c0bdea970
jarek1696 <jarek1696>
2019-09-17 08:38:12 +0200

    change path to account controller endpoints

--- src/main/java/pl/com/tt/intern/soccer/controller/AccountController.java
+++ src/main/java/pl/com/tt/intern/soccer/controller/AccountController.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
-@RequestMapping("/account")
+@RequestMapping("/accounts")
 @RequiredArgsConstructor
 public class AccountController {
 
