283ab209aaa4a118c7faec6c1c05fcaec71260f2
wojcik.patryk <wojcik.patrykk@gmail.com>
2019-09-19 08:56:37 +0200

    PLUB19-175 Add some reservation controller tests

--- src/test/java/pl/com/tt/intern/soccer/ReservationControllerTests.groovy
+++ src/test/java/pl/com/tt/intern/soccer/ReservationControllerTests.groovy
@@ -1,53 +1,95 @@
 package pl.com.tt.intern.soccer
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+
+import org.springframework.security.test.context.support.WithMockUser
 import pl.com.tt.intern.soccer.controller.ReservationController
 import pl.com.tt.intern.soccer.payload.request.ReservationDateRequest
+import pl.com.tt.intern.soccer.payload.response.ReservationResponse
+import pl.com.tt.intern.soccer.service.ReservationService
 import spock.lang.Specification
 
 import static java.time.DayOfWeek.MONDAY
+import static java.time.DayOfWeek.SUNDAY
 import static java.time.LocalDateTime.now
 import static org.springframework.http.HttpStatus.OK
 import static pl.com.tt.intern.soccer.model.enums.ReservationPeriod.TODAY
 
-@SpringBootTest
+@WithMockUser
 class ReservationControllerTests extends Specification {
 
-    @Autowired
-    ReservationController controller
+    ReservationService service = Mock()
+    ReservationController controller = new ReservationController(service)
 
-    def "should findByPeriod method return OK status"() {
+    def "findByPeriod method should return OK status"() {
         given:
         def period = TODAY
 
         when:
-        def result = controller.findByPeriod(period).getStatusCode()
+        def status = controller.findByPeriod(period).getStatusCode()
 
         then:
-        result == OK
+        status == OK
     }
 
-    def "should findByDay method return OK status"() {
+    def "findByDay method should return OK status"() {
         given:
         def day = MONDAY
 
         when:
-        def result = controller.findByDay(day).getStatusCode()
+        def status = controller.findByDay(day).getStatusCode()
 
         then:
-        result == OK
+        status == OK
     }
 
-    def "should findByDate method return OK status"() {
+    def "findByDate method should return OK status"() {
         given:
         def date = new ReservationDateRequest(now().withHour(10), now().withHour(20))
 
         when:
-        def result = controller.findByDate(date).getStatusCode()
+        def status = controller.findByDate(date).getStatusCode()
 
         then:
-        result == OK
+        status == OK
+    }
+
+    def "findByPeriod method should return list of ReservationResponse"() {
+        given:
+        def period = TODAY
+        def list = new ArrayList<ReservationResponse>()
+
+        when:
+        def body = controller.findByPeriod(period).getBody()
+
+        then:
+        1 * service.findByPeriod(period) >> list
+        body == list
+    }
+
+    def "findByDay method should return list of ReservationResponse"() {
+        given:
+        def day = SUNDAY
+        def list = new ArrayList<ReservationResponse>()
+
+        when:
+        def body = controller.findByDay(day).getBody()
+
+        then:
+        1 * service.findByDay(day) >> list
+        body == list
+    }
+
+    def "findByDate method should return list of ReservationResponse"() {
+        given:
+        def date = new ReservationDateRequest(now().withHour(10), now().withHour(20))
+        def list = new ArrayList<ReservationResponse>()
+
+        when:
+        def body = controller.findByDate(date).getBody()
+
+        then:
+        1 * service.findByDateBetween(date) >> list
+        body == list
     }
 
 }
