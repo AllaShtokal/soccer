cad0a00ccceb38fc5d163b8b5df2d7002e1fd1a9
Kamil Wawer <wawerk@tt.com.pl>
2019-09-23 17:31:37 +0200

    PLUB19-179 Add confirmation model and liquibase script

---
+++ src/main/java/pl/com/tt/intern/soccer/model/ConfirmationReservation.java
@@ -0,0 +1,55 @@
+package pl.com.tt.intern.soccer.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import pl.com.tt.intern.soccer.payload.response.ReservationResponse;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "confirmation_reservation")
+@EqualsAndHashCode(exclude = {"reservation"})
+public class ConfirmationReservation {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "reservation_id", nullable = false)
+    private Reservation reservation;
+
+    @NotNull
+    @Column(name = "uuid", unique = true)
+    private String uuid;
+
+    @NotNull
+    @Column(name = "expiration_time")
+    private LocalDateTime expirationTime;
+
+    @NotNull
+    @Column(name = "time_to_mail_send")
+    private LocalDateTime timeToMailSend;
+
+    @Column(name = "email_sent")
+    private Boolean emailSent;
+
+    public ConfirmationReservation(Reservation reservation) {
+        this.reservation = reservation;
+        this.uuid = UUID.randomUUID().toString();
+        this.expirationTime = LocalDateTime.now().minusMinutes(3);
+        this.timeToMailSend = LocalDateTime.now().withMinute(1);
+        this.emailSent = false;
+    }
+}
