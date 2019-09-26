e026fa674114fcc0526bbf7bbc402977bf12c8d2
jarek1696 <jarek1696>
2019-09-26 11:43:43 +0200

    Add abstract class for url generator and isolate repetitive methods into one class

--- src/main/java/pl/com/tt/intern/soccer/account/url/impl/AccountActivationUrlGenerator.java
+++ src/main/java/pl/com/tt/intern/soccer/account/url/impl/AccountActivationUrlGenerator.java
@@ -1,30 +1,22 @@
 package pl.com.tt.intern.soccer.account.url.impl;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.com.tt.intern.soccer.account.factory.AccountChangeType;
+import pl.com.tt.intern.soccer.account.url.AbstractAccountChangeUrlGenerator;
 import pl.com.tt.intern.soccer.account.url.AccountChangeUrlGenerator;
-import pl.com.tt.intern.soccer.account.url.enums.UrlParam;
-import pl.com.tt.intern.soccer.account.url.util.UrlGeneratorHelper;
-
-import java.util.Map;
 
 import static pl.com.tt.intern.soccer.account.factory.AccountChangeType.ACTIVE_ACCOUNT;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class AccountActivationUrlGenerator implements AccountChangeUrlGenerator {
+public class AccountActivationUrlGenerator extends AbstractAccountChangeUrlGenerator implements AccountChangeUrlGenerator {
 
+    @Getter
     private final String urlSuffix = "login";
-
-    @SneakyThrows
-    @Override
-    public String generate(Map<UrlParam, String> params) {
-        return UrlGeneratorHelper.createUrl(urlSuffix, params);
-    }
 
     @Override
     public boolean supports(AccountChangeType type) {
