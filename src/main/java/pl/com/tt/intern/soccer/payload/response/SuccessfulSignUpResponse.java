ffbac5620ebb74bcccb3a602d995964cea4f6076
jarek1696 <jarek1696>
2019-09-27 08:16:57 +0200

    Added inclusion of sending e-mails and return key in json after signup

--- src/main/java/pl/com/tt/intern/soccer/payload/response/SuccessfulSignUpResponse.java
+++ src/main/java/pl/com/tt/intern/soccer/payload/response/SuccessfulSignUpResponse.java
@@ -7,15 +7,16 @@ import pl.com.tt.intern.soccer.model.User;
 public class SuccessfulSignUpResponse {
 
     private String message;
+    private String activeKey;
     private String firstName;
     private String lastName;
     private String email;
 
-    public SuccessfulSignUpResponse(String message, User user) {
+    public SuccessfulSignUpResponse(String message, String activeKey, User user) {
         this.message = message;
+        this.activeKey = activeKey;
         this.firstName = user.getUserInfo().getFirstName();
         this.lastName = user.getUserInfo().getLastName();
         this.email = user.getEmail();
     }
-
 }
