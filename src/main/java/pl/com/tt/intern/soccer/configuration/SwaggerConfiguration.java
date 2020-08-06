dfc44c94767c3be2758a4113ffe366cab6f954a5
Mateusz Gliwka <mateusz.gliwka@ttsw.com.pl>
2020-08-06 15:39:47 +0200

    PLUB19-250

--- src/main/java/pl/com/tt/intern/soccer/configuration/SwaggerConfiguration.java
+++ src/main/java/pl/com/tt/intern/soccer/configuration/SwaggerConfiguration.java
@@ -2,9 +2,20 @@ package pl.com.tt.intern.soccer.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.com.tt.intern.soccer.annotation.CurrentUser;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
+import static io.swagger.models.auth.In.HEADER;
+import static java.util.Collections.singletonList;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static springfox.documentation.builders.PathSelectors.any;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
@@ -18,10 +29,22 @@ public class SwaggerConfiguration {
     @Bean
     public Docket docket() {
         return new Docket(SWAGGER_2)
+                .ignoredParameterTypes(CurrentUser.class)
+                .securitySchemes(singletonList(new ApiKey("JWT", AUTHORIZATION, HEADER.name())))
+                .securityContexts(singletonList(
+                        SecurityContext.builder()
+                                .securityReferences(
+                                        singletonList(SecurityReference.builder()
+                                                .reference("JWT")
+                                                .scopes(new AuthorizationScope[0])
+                                                .build()
+                                        )
+                                )
+                                .build())
+                )
                 .select()
                 .apis(basePackage(BASE_PACKAGE))
                 .paths(any())
                 .build();
     }
-
 }
